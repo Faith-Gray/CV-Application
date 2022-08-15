3d15b76a5028dae0ef7fdd9e9541c47dabb29521
Faith <fhgray96@gmail.com>
2022-08-15 16:13:09 -0500

    List out components. Add form for Header to enter candidate's personal information

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import HeaderForm from './components/HeaderForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <HeaderForm />
 
     </div>
   )
 }
 
 export default App
+
+
+//components
+//Header - Name, job title? phone, email, location
+
+//Work Experience - Job Component - be able to add and delete these
+//job title, company, dates, bullet points
+
+//Education - School, Dates - be able to add and delete
---
+++ src/components/HeaderForm.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function HeaderForm() {
+    const [ resumeName, resumeTitle, phoneNumber, email, location ] = useState('');
+
+    function handleChange(e) {
+        setFormInput(e.currentTarget.value)
+    };
+
+    return (
+        <form>
+            <input value={resumeName} type="text" onChange={handleChange} placeholder="Name"/>
+            <input value={resumeTitle} type="text" onChange={handleChange} placeholder="Title"/>
+            <input value={phoneNumber} type="text" onChange={handleChange} placeholder="Phone Number"/>
+            <input value={email} type="text" onChange={handleChange} placeholder="Email"/>
+            <input value={location} type="text" onChange={handleChange} placeholder="Location"/>
+        </form>
+    );
+};
+  
+export default HeaderForm;
