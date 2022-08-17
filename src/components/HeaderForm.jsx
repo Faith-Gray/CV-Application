70e8777c10936d5342cd82bf841fa2c9ee4fbda7
Faith <fhgray96@gmail.com>
2022-08-16 22:31:33 -0500

    completed Header controlled form with state

--- src/components/HeaderForm.jsx
+++ src/components/HeaderForm.jsx
@@ -5,17 +5,54 @@ function HeaderForm() {
         {resumeName: "", resumeTitle: "", phoneNumber: "", email: "", location: ""}
     );
 
+    console.log(headerFormData);
+
     function handleChange(e) {
-        setFormInput(e.currentTarget.value)
+        setHeaderFormData(prevHeaderFormData => {
+            return {
+                ...prevHeaderFormData,
+                [e.target.name]: e.target.value
+            }
+        })
     };
 
     return (
         <form>
-            <input value={resumeName} type="text" onChange={handleChange} placeholder="Name"/>
-            <input value={resumeTitle} type="text" onChange={handleChange} placeholder="Title"/>
-            <input value={phoneNumber} type="text" onChange={handleChange} placeholder="Phone Number"/>
-            <input value={email} type="text" onChange={handleChange} placeholder="Email"/>
-            <input value={location} type="text" onChange={handleChange} placeholder="Location"/>
+            <input 
+                // value={resumeName} 
+                type="text" 
+                onChange={handleChange} 
+                placeholder="Name"
+                name="resumeName"
+            />
+            <input 
+                // value={resumeTitle} 
+                type="text" 
+                onChange={handleChange} 
+                placeholder="Title"
+                name="resumeTitle"
+            />
+            <input 
+                // value={phoneNumber} 
+                type="text" 
+                onChange={handleChange} 
+                placeholder="Phone Number"
+                name="phoneNumber"
+            />
+            <input 
+                // value={email} 
+                type="text" 
+                onChange={handleChange} 
+                placeholder="Email"
+                name="email"
+            />
+            <input 
+                // value={location} 
+                type="text" 
+                onChange={handleChange} 
+                placeholder="Location"
+                name="location"
+            />
         </form>
     );
 };
