926507d70947668d66941c045fd8a05fd78675b7
Faith <fhgray96@gmail.com>
2022-08-20 21:40:21 -0500

    update form style

--- src/Form/PersonalDetailsForm.jsx
+++ src/Form/PersonalDetailsForm.jsx
@@ -17,43 +17,46 @@ function PersonalDetailsForm( { PersonalDetailsFormData, handleChange } ) {
     // };
 
     return (
-        <form>
-            <input 
-                value={PersonalDetailsFormData.resumeName} 
-                type="text" 
-                onChange={handleChange} 
-                placeholder="Name"
-                name="resumeName"
-            />
-            <input 
-                value={PersonalDetailsFormData.resumeTitle} 
-                type="text" 
-                onChange={handleChange} 
-                placeholder="Title"
-                name="resumeTitle"
-            />
-            <input 
-                value={PersonalDetailsFormData.phoneNumber} 
-                type="text" 
-                onChange={handleChange} 
-                placeholder="Phone Number"
-                name="phoneNumber"
-            />
-            <input 
-                value={PersonalDetailsFormData.email} 
-                type="text" 
-                onChange={handleChange} 
-                placeholder="Email"
-                name="email"
-            />
-            <input 
-                value={PersonalDetailsFormData.location} 
-                type="text" 
-                onChange={handleChange} 
-                placeholder="Location"
-                name="location"
-            />
-        </form>
+        <>
+            <div>Personal Details</div>
+            <form>
+                <input 
+                    value={PersonalDetailsFormData.resumeName} 
+                    type="text" 
+                    onChange={handleChange} 
+                    placeholder="Name"
+                    name="resumeName"
+                />
+                <input 
+                    value={PersonalDetailsFormData.resumeTitle} 
+                    type="text" 
+                    onChange={handleChange} 
+                    placeholder="Title"
+                    name="resumeTitle"
+                />
+                <input 
+                    value={PersonalDetailsFormData.phoneNumber} 
+                    type="text" 
+                    onChange={handleChange} 
+                    placeholder="Phone Number"
+                    name="phoneNumber"
+                />
+                <input 
+                    value={PersonalDetailsFormData.email} 
+                    type="text" 
+                    onChange={handleChange} 
+                    placeholder="Email"
+                    name="email"
+                />
+                <input 
+                    value={PersonalDetailsFormData.location} 
+                    type="text" 
+                    onChange={handleChange} 
+                    placeholder="Location"
+                    name="location"
+                />
+            </form>
+        </>
     );
 
 };
